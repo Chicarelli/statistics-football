aa9dfc59d6c2160e43e72c6ef5483b1344d9f885
Rafael Chicarellil <rafaelsep20@gmail.com>
2021-07-24 11:48:38 -0300

    Refatorado método de pegar todos os times rastreados no campeonato e salvar numa tabela

--- controllers/establishingTeams.js
+++ controllers/establishingTeams.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const db = require("typeorm");
 
 //Método para descobrir os times a serem procurados. Precisa realizar dois tipos de consultas e depois somar tudo em um array. 
 //Os dados trazidos de tabelas capturadas de copa/campeonato serão diferentes. 
@@ -7,25 +7,22 @@ const establishingTeams = async () => {
     arrayDeTimes = [];
     classificacao = [];
 
-    var resultad = await db.promise().query('SELECT classificacao FROM football.campeonato WHERE type = "campeonato"')
-        .then(result => {
-            result.map(item => {
-                item.map(i => {
-                    if (i.classificacao) {
-                        classificacao.push(JSON.parse(i.classificacao));
-                    }
-                })
-            })
-            return classificacao
-        }
-        )
-        .catch(error => console.log(error))
-    classificacao.forEach(item => {
-        let timesClassificacao = Object.values(item);
-        timesClassificacao.forEach(item => arrayDeTimes.push(item.time));
-    });
+    await db.getConnection()
+    .createQueryBuilder()
+    .select("classificacao")
+    .from("campeonato")
+    .where("type = :type", {type: "campeonato"})
+    .execute()
+    .then(result => {
+        result.map(item => {
+            teamsParsed = Object.entries(JSON.parse(item.classificacao));
+            teamsParsed.map(team => classificacao.push(team[1].time));
+        })
+        return classificacao;
+    })
+    .catch(error => console.log(error));
 
-    arrayDeTimes = [...new Set(arrayDeTimes)];
+    arrayDeTimes = [...new Set(classificacao)];
 
     return arrayDeTimes;
 }
@@ -35,42 +32,29 @@ const gatheringTeamsCups = async () => {
     let arrayTimesCopa = [];
     let classificacao = [];
     //Procurar times de Copas para reunir com os times de campeonatos já recebido. 
-
-    await db.promise().query('SELECT classificacao FROM football.campeonato WHERE type = "copa"')
-        .then(result => {
-            result.map(item => {
-                item.map(i => {
-                    if (i.classificacao) {
-                        classificacao.push(JSON.parse(i.classificacao));
-                    }
-                })
-            }
-
-                // copas = result.map(item => JSON.parse(item.classificacao));
-            )
-        })
-        .catch(errors => console.log(errors));
-
     
-        classificacao.forEach(vettor => {
-            vettor.forEach(vetor => {
-                vetor.forEach(copa => {
-                    arrayTimesCopa.push(copa.club);
-                })
-            })
+    await db.getConnection().createQueryBuilder()
+    .select("classificacao")
+    .from("campeonato")
+    .where("type = :type", {type: "copa"})
+    .execute()
+    .then(result => {
+        result.map(item => {
+            teamsParsed = JSON.parse(item.classificacao);
+            teamsParsed.map(teamGroup=>{
+                classificacao.push(teamGroup.map(team => team.club));
+            });
         })
+    })
+    .catch(errors => console.log(errors));
+
+    classificacao.forEach(group => {
+        group.map(item => arrayTimesCopa.push(item));
+    });
    
     arrayTimesCopa = [...new Set(arrayTimesCopa)];
     return arrayTimesCopa
 }
-
-// const juntandoArrays = (arrayCampeonato, arrayCopa) => {
-//     let newArray = [...arrayCampeonato, ...arrayCopa];
-//     let arraySemDuplicatas = [...new Set(newArray)];
-
-//     return arraySemDuplicatas.sort();
-// }
-
 
 module.exports = {
     timesCampeonato: establishingTeams,
--- database/teams.js
+++ database/teams.js
@@ -1,32 +1,40 @@
-const db = require('../db');
+const connection = require('../db');
+const db = require("typeorm");
 const {timesCampeonato, timesCopa} = require('../controllers/establishingTeams');
 
 async function buscarTimes() {
+
+    await connection();
+
     const timesDeCampeonato = await timesCampeonato();
     const timesDeCopas = await timesCopa();
+    // console.log(timesDeCampeonato);
 
     todosOsTimes = [...timesDeCampeonato, ...timesDeCopas];
     todosOsTimes = [...new Set(todosOsTimes)];
-
-    console.log(todosOsTimes);
     
     salvarTimes(todosOsTimes);
 }
 buscarTimes();
 
-function salvarTimes(times){
+async function salvarTimes(times){
     for(const time of times){
-        db.query('SELECT nome FROM football.times WHERE nome = ?', [time], 
-            function (err, result){
-                if(result.length == 0){
-                    db.query('INSERT INTO football.times (nome) values (?)', [time], 
-                        function (err, result){
-                            console.log('err', err);
-                            console.log('result', result);
-                        }
-                    )
-                }
+
+        await db.getConnection().createQueryBuilder()
+        .select("nome")
+        .from("times")
+        .where("nome = :nome", {nome: time})
+        .execute()
+        .then(async result => {
+            if(result.lenght == 0){
+                await db.getConnection().createQueryBuilder()
+                .insert()
+                .into("times")
+                .values([
+                    {nome: time}
+                ])
+                .execute();
             }
-        )
+        });
     }
 }
